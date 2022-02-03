f0ac06b756b789331e88c1799205a215647de396
Ivan <redial@abv.bg>
2022-02-03 21:41:43 +0200

    static root added

--- balkanultra/settings.py
+++ balkanultra/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/dist/'
-# STATIC_ROOT = "" # STATIC_ROOT is the folder where static files will be stored after using manage.py 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
     BASE_DIR / "dist"
